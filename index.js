06b0d59eac324c583061449b1bffebc74c07182f
tothricsaj <tothricsaj@gmail.com>
2021-11-23 18:40:59 +0100

    feat: colored output

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const { client } = require('./rssClient');
 
 
 function main() {
-	// input();
-	client();
+	input();
 }
 
 main();
--- rssClient.js
+++ rssClient.js
@@ -1,11 +1,12 @@
 const https = require('https');
 const parseString = require('xml2js').parseString;
+const { colors } = require('./colors');
 
 let xmlBuff = '';
 let feedArray = [];
 
 exports.client = (host, path) => {
-	https.get({host: 'telex.hu', path: '/rss'}, res => {
+	https.get({host: host, path: path}, res => {
 
 		res.on('data', data => {
 			xmlBuff += data
@@ -17,11 +18,11 @@ exports.client = (host, path) => {
 				feedArray = [...result['rss']['channel'][0]["item"]];
 
 				feedArray.forEach(element => {
-					console.log('Title: ', element.title[0]);
-					console.log('Description: ', element.description[0]);
-					console.log('Link: ', element.link[0]);
+					console.log(`${colors.Bright}${colors.FgGreen}${element.title[0]}${colors.Reset}\n`);
+					console.log(`${colors.FgGreen}${element.description[0]}${colors.Reset}`);
+					console.log(`${element.link[0]}`);
 
-					console.log('\n------------------------oooooooooooooooo------------------------\n');
+					console.log(`\n${colors.BgRed}${colors.FgWhite}------------------------oooooooooooooooo------------------------${colors.Reset}\n`);
 				});
 			});
 			process.exit(0)
